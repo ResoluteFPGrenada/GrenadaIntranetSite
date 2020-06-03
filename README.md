841394a8073606c7a26ddf85a3976c6b4373bda9
Hodges <Jason.Hodges@resolutefp.com>
2020-06-03 12:44:08 -0500

    completed functionality to setup.ps1 and corrected setup instructions

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ A intranet site used to centralize data and applications in one local location a
 
 3. move setup.ps1 file to a directory where the site should be built.
 
-4. run setup.ps1 as admin and with powershell execution-policy to remote or bypass.
+4. open command prompt as administrator and cd to project folder
 
-5. run python manage.py prod in project directory to run server in production environment. otherwise python manage.py dev in testing/dev environment.
+5. run powershell .\setup.ps1 executionpolicy bypass
+
+6. add databases from backup directory to PROJECTFOLDER\WebApp\databases
+
+7. run venv\scripts\activate
+
+8. run python manage.py runserver prod to run server in production environment. otherwise python manage.py runserver dev in testing/dev environment.
--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 #Setup#
 new-item -Path "$PSScriptRoot\log.txt" -type File -force
-write-host "creating log file"
+write-warning "creating log file"
 
 # Create local git repo #
 git init
@@ -20,43 +20,45 @@ git remote add origin https://github.com/ResolutefpGrenada/GrenadaIntranetSite.g
 
 
 # pull down repo #
-<#
+
 git pull origin master
-#>
+
 
 # check if virtualenv is installed #
-<#
+
 try{
     virtualenv --version
 }catch{
     pip install virtualenv 
-    write-host "installing virtualenv on local machine."
+    write-warning "installing virtualenv on local machine."
     add-content "$PSScriptRoot\log.txt" -Value "Installed virtualenv." -force
     read-host "Press any button to continue"
 }
-#>
+
 
 # setup virtual environment #
-<#
+
 $TP = test-path .\venv
 if($TP -ne $true){
     virtualenv venv
 }
 
 venv\scripts\activate
-#>
+
 
 # run requirments.txt to install denpendencies #
-<#
+
 pip install -r requirements.txt
-#>
+
 
 # Ask user to create scheduled task to run python manage.py prod in directory folder. #
-<#
-write-host "Please create scheduled task to run python manage.py prod in project directory."
+write-host ""
+write-warning "Please create scheduled task to run python manage.py prod in project directory."
 read-host "Press any button to continue"
-#>
 
-# Create and Write to log.txt to show setup was successful. # 
 
+# Deactivate venv and show setup was successful. # 
+venv\scripts\deactivate.bat
+write-warning "Setup of site was successful."
 add-content "$PSScriptRoot\log.txt" -Value "Setup was completed successfully." -force
+read-host "Press any button to complete this setup."
