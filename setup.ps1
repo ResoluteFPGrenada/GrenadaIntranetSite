c58a5d5decd05b529a3aead6999d8b743b875775
Hodges <Jason.Hodges@resolutefp.com>
2020-06-03 12:08:52 -0500

    Making adding functionality to setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,62 @@
+#Setup#
+new-item -Path "$PSScriptRoot\log.txt" -type File -force
+write-host "creating log file"
+
+# Create local git repo #
+git init
+
+# Check if Python is installed #
+try{
+    python --version
+}catch{
+    write-error "Please install python 3.6 or higher, before running this setup tool. Otherwise make sure python in setup in path"
+    add-content "$PSScriptRoot\log.txt" -Value "Python not installed or path could not be found. Please install Python 3.6 or later." -force
+    read-host "Press any button to stop program."
+    break
+}
+
+# setup connection to github repo #
+git remote add origin https://github.com/ResolutefpGrenada/GrenadaIntranetSite.git
+
+
+# pull down repo #
+<#
+git pull origin master
+#>
+
+# check if virtualenv is installed #
+<#
+try{
+    virtualenv --version
+}catch{
+    pip install virtualenv 
+    write-host "installing virtualenv on local machine."
+    add-content "$PSScriptRoot\log.txt" -Value "Installed virtualenv." -force
+    read-host "Press any button to continue"
+}
+#>
+
+# setup virtual environment #
+<#
+$TP = test-path .\venv
+if($TP -ne $true){
+    virtualenv venv
+}
+
+venv\scripts\activate
+#>
+
+# run requirments.txt to install denpendencies #
+<#
+pip install -r requirements.txt
+#>
+
+# Ask user to create scheduled task to run python manage.py prod in directory folder. #
+<#
+write-host "Please create scheduled task to run python manage.py prod in project directory."
+read-host "Press any button to continue"
+#>
+
+# Create and Write to log.txt to show setup was successful. # 
+
+add-content "$PSScriptRoot\log.txt" -Value "Setup was completed successfully." -force
